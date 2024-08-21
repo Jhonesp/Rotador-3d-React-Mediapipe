e1d54f1dd981c7a0dc26d1ac3d0dd3d74f9f17ed
Jhon Esparragoza <jhonantonyesp@gmail.com>
2024-08-21 11:49:34 +0200

    e

--- src/HandControl.jsx
+++ src/HandControl.jsx
@@ -8,35 +8,42 @@ const HandControl = () => {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    const hands = new Hands({
-      locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`,
-    });
+    const initMediaPipe = async () => {
+      const hands = new Hands({
+        locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`,
+      });
 
-    hands.setOptions({
-      maxNumHands: 1,
-      modelComplexity: 1,
-      minDetectionConfidence: 0.5,
-      minTrackingConfidence: 0.5,
-    });
+      hands.setOptions({
+        maxNumHands: 1,
+        modelComplexity: 1,
+        minDetectionConfidence: 0.5,
+        minTrackingConfidence: 0.5,
+      });
 
-    hands.onResults((results) => {
-      if (results.multiHandLandmarks.length > 0) {
-        const landmarks = results.multiHandLandmarks[0];
-        const xRotation = (landmarks[0].x - 0.5) * 2 * Math.PI;
-        const yRotation = (landmarks[0].y - 0.5) * 2 * Math.PI;
-        setRotation({ x: yRotation, y: xRotation });
-      }
-    });
+      // Carga de los recursos antes de comenzar
+      await hands.initialize();
 
-    const camera = new Camera(videoRef.current, {
-      onFrame: async () => {
-        await hands.send({ image: videoRef.current });
-      },
-      width: 640,
-      height: 480,
-    });
+      hands.onResults((results) => {
+        if (results.multiHandLandmarks.length > 0) {
+          const landmarks = results.multiHandLandmarks[0];
+          const xRotation = (landmarks[0].x - 0.5) * 2 * Math.PI;
+          const yRotation = (landmarks[0].y - 0.5) * 2 * Math.PI;
+          setRotation({ x: yRotation, y: xRotation });
+        }
+      });
 
-    camera.start();
+      const camera = new Camera(videoRef.current, {
+        onFrame: async () => {
+          await hands.send({ image: videoRef.current });
+        },
+        width: 640,
+        height: 480,
+      });
+
+      camera.start();
+    };
+
+    initMediaPipe();
   }, []);
 
   return (
