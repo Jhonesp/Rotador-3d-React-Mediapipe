a79618f727407c85b20eabaf5ae976c279a13de8
Jhon Esparragoza <jhonantonyesp@gmail.com>
2024-08-21 12:07:51 +0200

    a

--- src/HandControl.jsx
+++ src/HandControl.jsx
@@ -8,42 +8,35 @@ const HandControl = () => {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    const initMediaPipe = async () => {
-      const hands = new Hands({
-        locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`,
-      });
+    const hands = new Hands({
+      locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`,
+    });
 
-      hands.setOptions({
-        maxNumHands: 1,
-        modelComplexity: 1,
-        minDetectionConfidence: 0.5,
-        minTrackingConfidence: 0.5,
-      });
+    hands.setOptions({
+      maxNumHands: 1,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+    });
 
-      // Carga de los recursos antes de comenzar
-      await hands.initialize();
+    hands.onResults((results) => {
+      if (results.multiHandLandmarks.length > 0) {
+        const landmarks = results.multiHandLandmarks[0];
+        const xRotation = (landmarks[0].x - 0.5) * 2 * Math.PI;
+        const yRotation = (landmarks[0].y - 0.5) * 2 * Math.PI;
+        setRotation({ x: yRotation, y: xRotation });
+      }
+    });
 
-      hands.onResults((results) => {
-        if (results.multiHandLandmarks.length > 0) {
-          const landmarks = results.multiHandLandmarks[0];
-          const xRotation = (landmarks[0].x - 0.5) * 2 * Math.PI;
-          const yRotation = (landmarks[0].y - 0.5) * 2 * Math.PI;
-          setRotation({ x: yRotation, y: xRotation });
-        }
-      });
+    const camera = new Camera(videoRef.current, {
+      onFrame: async () => {
+        await hands.send({ image: videoRef.current });
+      },
+      width: 640,
+      height: 480,
+    });
 
-      const camera = new Camera(videoRef.current, {
-        onFrame: async () => {
-          await hands.send({ image: videoRef.current });
-        },
-        width: 640,
-        height: 480,
-      });
-
-      camera.start();
-    };
-
-    initMediaPipe();
+    camera.start();
   }, []);
 
   return (
